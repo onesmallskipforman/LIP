ce9877c903dfafb49a0fab9a8575c9004ee5105e
onesmallskipforman <skipper.gonzalez@icloud.com>
2024-09-28 13:01:47 -0400

    Get blink from scratch working

---
+++ stm32_testing/blinky-from-scratch/main.c
@@ -0,0 +1,89 @@
+#include <stdint.h>
+#define RCC_BASE 0x40021000
+#define RCC_APB2ENR_REGISTER (*(volatile uint32_t *)(RCC_BASE + 0x18))
+#define RCC_APB2ENR_IOPCEN (1 << 4)
+#define RCC_APB2ENR_IOPGEN (1 << 8)
+#define RCC_APB2ENR_IOPAEN (1 << 2)
+
+
+
+
+#define GPIO_PORTC_BASE 0x40011000
+#define GPIO_PORTG_BASE 0x40012000
+#define GPIO_PORTA_BASE 0x40010800
+#define GPIO_CRH_REGISTER(x) (*(volatile uint32_t *)(x + 0x4))
+#define GPIO_CRL_REGISTER(x) (*(volatile uint32_t *)(x + 0x0))
+#define GPIO_CRH_MODE_MASK(x) (0x3 << ((x - 8) * 4))
+#define GPIO_CRH_MODE_OUTPUT(x) (0x1 << ((x - 8) * 4))
+#define GPIO_CRL_MODE_MASK(x) (0x3 << (x * 4))
+#define GPIO_CRL_MODE_OUTPUT(x) (0x1 << (x * 4))
+#define GPIOC_BLINK_PORT GPIO_PORTC_BASE
+#define GPIOG_BLINK_PORT GPIO_PORTG_BASE
+#define GPIOA_BLINK_PORT GPIO_PORTA_BASE
+#define GPIOC_BLINK_NUM 13
+#define GPIOG_BLINK_NUM 4
+#define GPIOA_BLINK_NUM 4
+
+#define GPIO_ODR_REGISTER(x) (*(volatile uint32_t *)(x + 0xC))
+#define GPIO_ODR_PIN(x) (1 << x)
+
+int main(void) {
+    // Enable port C clock gate.
+    RCC_APB2ENR_REGISTER |= RCC_APB2ENR_IOPCEN;
+    RCC_APB2ENR_REGISTER |= RCC_APB2ENR_IOPGEN;
+    RCC_APB2ENR_REGISTER |= RCC_APB2ENR_IOPAEN;
+
+
+    // Configure GPIO C pin 13 as output.
+    GPIO_CRH_REGISTER(GPIOC_BLINK_PORT) &= ~(GPIO_CRH_MODE_MASK(GPIOC_BLINK_NUM));
+    GPIO_CRH_REGISTER(GPIOC_BLINK_PORT) |= GPIO_CRH_MODE_OUTPUT(GPIOC_BLINK_NUM);
+    // Configure GPIO G pin 4 as output.
+    GPIO_CRL_REGISTER(GPIOG_BLINK_PORT) &= ~(GPIO_CRL_MODE_MASK(GPIOG_BLINK_NUM));
+    GPIO_CRL_REGISTER(GPIOG_BLINK_PORT) |= GPIO_CRL_MODE_OUTPUT(GPIOG_BLINK_NUM);
+    // Configure GPIO A pin 4 as output.
+    GPIO_CRL_REGISTER(GPIOA_BLINK_PORT) &= ~(GPIO_CRL_MODE_MASK(GPIOA_BLINK_NUM));
+    GPIO_CRL_REGISTER(GPIOA_BLINK_PORT) |= GPIO_CRL_MODE_OUTPUT(GPIOA_BLINK_NUM);
+
+    GPIO_ODR_REGISTER(GPIOG_BLINK_PORT) = 1;
+    GPIO_ODR_REGISTER(GPIOC_BLINK_PORT) = 1;
+    GPIO_ODR_REGISTER(GPIOA_BLINK_PORT) = 1;
+    for (;;) {
+        // Set the output bit.
+        GPIO_ODR_REGISTER(GPIOC_BLINK_PORT) |= GPIO_ODR_PIN(GPIOC_BLINK_NUM);
+        for (uint32_t i = 0; i < 400000; ++i) {
+            __asm__ volatile("nop");
+        }
+        // Reset it again.
+        GPIO_ODR_REGISTER(GPIOC_BLINK_PORT) &= ~GPIO_ODR_PIN(GPIOC_BLINK_NUM);
+        for (uint32_t i = 0; i < 10000; ++i) {
+            __asm__ volatile("nop");
+        }
+
+
+
+        // Set the output bit.
+        GPIO_ODR_REGISTER(GPIOG_BLINK_PORT) |= GPIO_ODR_PIN(GPIOG_BLINK_NUM);
+        for (uint32_t i = 0; i < 400000; ++i) {
+            __asm__ volatile("nop");
+        }
+        // Reset it again.
+        GPIO_ODR_REGISTER(GPIOG_BLINK_PORT) &= ~GPIO_ODR_PIN(GPIOG_BLINK_NUM);
+        for (uint32_t i = 0; i < 10000; ++i) {
+            __asm__ volatile("nop");
+        }
+
+
+        // Set the output bit.
+        GPIO_ODR_REGISTER(GPIOA_BLINK_PORT) |= GPIO_ODR_PIN(GPIOA_BLINK_NUM);
+        for (uint32_t i = 0; i < 400000; ++i) {
+            __asm__ volatile("nop");
+        }
+        // Reset it again.
+        GPIO_ODR_REGISTER(GPIOA_BLINK_PORT) &= ~GPIO_ODR_PIN(GPIOA_BLINK_NUM);
+        for (uint32_t i = 0; i < 10000; ++i) {
+            __asm__ volatile("nop");
+        }
+    }
+
+    return 0;
+}
