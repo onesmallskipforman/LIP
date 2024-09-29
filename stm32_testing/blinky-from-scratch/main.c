2c08c778377e9911d9e45f941fa408b64c6bfe12
onesmallskipforman <skipper.gonzalez@icloud.com>
2024-09-29 12:14:09 -0400

    Get blink working with stm linker script

--- stm32_testing/blinky-from-scratch/main.c
+++ stm32_testing/blinky-from-scratch/main.c
@@ -27,6 +27,40 @@
 #define GPIO_ODR_REGISTER(x) (*(volatile uint32_t *)(x + 0xC))
 #define GPIO_ODR_PIN(x) (1 << x)
 
+
+
+
+// Startup code
+__attribute__((naked, noreturn)) void _reset(void) {
+  // Initialise memory
+  extern long _sbss, _ebss, _sdata, _edata, _sidata;
+  for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
+  for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
+
+  // Call main()
+  extern int main(void);
+  main();
+  for (;;) (void) 0;  // Infinite loop
+}
+
+extern void SysTick_Handler(void);  // Defined in main.c
+extern void _reset_stack_pointer(void);          // Defined in link.ld
+
+// 16 standard and 91 STM32-specific handlers
+__attribute__((section(".vectors"))) void (*const tab[16 + 91])(void) = {
+    _reset_stack_pointer, _reset, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, SysTick_Handler};
+
+
+
+static volatile uint32_t s_ticks;
+void SysTick_Handler(void) {
+  s_ticks++;
+}
+
+
+
+
+
 void SystemInit(void) {
     return;
 }
